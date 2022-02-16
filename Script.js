2e8e606b72f1bd61b80b71ea7271c7f2be3fd634
RubensGabrielVelazquezFonseca <rubensfonsecaifpr@gmail.com>
2022-02-15 21:00:46 -0300

    testando alguma coisa

---
+++ Script.js
@@ -0,0 +1,7 @@
+const planets = document.querySelectorAll('.planet')
+const p_radii = [22,33,50,70,112,138,165,190]
+let p_radians = new Array(8).fill(0)
+const p_velocities = [1.607]
+
+
+const p_orbits = document.querySelectorAll('.p-orbit')
