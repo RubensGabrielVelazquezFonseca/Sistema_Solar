99b607463d13ec4fe74c44394f1789a41e5b5865
RubensGabrielVelazquezFonseca <rubensfonsecaifpr@gmail.com>
2022-02-23 07:48:19 -0300

    add novamnte outro teste

--- Script.js
+++ Script.js
@@ -1,27 +1,18 @@
-const planets = document.querySelectorAll('.planet')
-const p_radii = [22]
-let p_radians = new Array(1).fill(0)
-const p_velocities = [1.607]
+const planeta = document.querySelectorAll('.planeta')
+const raio = [80]
+const radiano = [0]
+const velocidade = [1.502]
+const orbita = document.querySelectorAll('.orbita')
 
-const p_orbits = document.querySelectorAll('.p-orbit')
-
-p_orbits.forEach((p_orbit, index)=>{
-  p_orbit.style.height = `${p_radii[index]}vmin`
-  p_orbit.style.width = `${p_radii[index]}vmin`
+orbita.forEach((orbita, index)=>{
+  orbita.style.height = `${raio[index]}vmin`
+  orbita.style.width = `${raio[index]}vmin`                 	
 })
 
 setInterval( ()=> {
-  planets.forEach( (planet, index)=>{
-    planet.style.left = `${Math.cos(p_radians[index]) * p_radii[index]}vmin`
-    planet.style.top = `${Math.sin(p_radians[index]) * p_radii[index]}vmin`
-    p_radians[index] += p_velocities[index] * 0.02
+  planeta.forEach( (planeta, index)=>{
+    planeta.style.left = `${Math.cos(radiano[index]) *   raio[index]}vmin`
+    planeta.style.top = `${Math.sin(radiano[index]) * raio[index]}vmin`
+    radiano[index] += velocidade[index] * 0.02
   })
-
-  moon.style.left = `${earthX() + (Math.cos(m_radians) * m_radius )}vmin`
-  moon.style.top = `${earthY() + (Math.sin(m_radians) * m_radius )}vmin`
-  m_radians += m_velocity * 0.02
-
-  m_orbit.style.left = `${earthX()}vmin`
-  m_orbit.style.top = `${earthY()}vmin`
 }, 1000/60)
-
