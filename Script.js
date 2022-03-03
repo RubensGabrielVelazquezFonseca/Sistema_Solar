7d8711faab39dcf47d0d90d521bf513c5ccdb1fb
RubensGabrielVelazquezFonseca <rubensfonsecaifpr@gmail.com>
2022-03-03 20:05:40 -0300

    up1

--- Script.js
+++ Script.js
@@ -1,8 +1,7 @@
 const planeta = document.getElementById("planeta");
-const velocidadeElement = document.getElementById("velocidade");
 const raioElement = document.getElementById("raio");
 const massaElement = document.getElementById("massa");
-let velocidade = velocidadeElement.value.replace(",", ".");
+let velocidade = 0;
 let massa = massaElement.value.replace(",", ".");
 let raio = raioElement.value;
 let radiano = 0;
@@ -14,14 +13,14 @@ orbita.style.width = `${raio}vmin`;
 setInterval(() => {
   planeta.style.left = `${Math.cos(radiano) * raio}vmin`;
   planeta.style.top = `${Math.sin(radiano) * raio}vmin`;
+  velocidade =  `${Math.sqrt (massa/raio)}`;
   radiano += velocidade * massa;
-}, 1000 / 60);
+}, 1000 / 60); 
 
 function mudarValores() {
   raio = raioElement.value;
-  velocidade = velocidadeElement.value.replace(",", ".");
   massa = massaElement.value.replace(",", ".");
   orbita.style.height = `${raio}vmin`;
   orbita.style.width = `${raio}vmin`;
-  console.log("valores", { raio, velocidade, massa });
+  console.log("valores", { raio, massa });
 }
